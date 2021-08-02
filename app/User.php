942c71bd90977b0520d73a87dd36fde2e1c31bbd
ahsanfurqan <ahsanfurqan.bukc@gmail.com>
2021-08-02 10:55:38 +0500

    new_change

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'nam', 'email', 'password',
+        'name', 'email', 'password',
     ];
 
     /**
